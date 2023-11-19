2226a0a1038fe6c83e9853122e7b6e85d62b9448
Joyce <lapinette2002@ymail.com>
2023-11-19 13:30:59 +0100

    WIP création de fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Activity;
+use App\Entity\Goal;
+use App\Entity\User;
+use App\Entity\WorkoutPlan;
+use App\Enum\GoalStatus;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création d'un utilisateur
+        $user = new User();
+        $user->setUsername('userdemo')
+            ->setEmail('user@example.com')
+            ->setPassword('password'); // En production, utilisez une méthode d'encodage de mot de passe
 
+        $manager->persist($user);
+
+        // Création d'une activité
+        $activity = new Activity();
+        $activity->setType('Course à pied')
+            ->setDistance(5.0)
+            ->setDuration(30)
+            ->setDate(new DateTime())
+            ->setAuthor($user);
+
+        $manager->persist($activity);
+
+        // Création d'un objectif
+        $goal = new Goal();
+        $goal->setDescription('Courir 10 km')
+            ->setDeadline(new DateTime('+1 month'))
+            ->setAuthor($user)
+            ->setStatus(GoalStatus::IN_PROGRESS);
+
+        $manager->persist($goal);
+
+        // Création d'un plan d'entraînement
+        $workoutPlan = new WorkoutPlan();
+        $workoutPlan->setDescription('Plan Marathon')
+            ->addActivity($activity);
+
+        $manager->persist($workoutPlan);
+
+        // Enregistrer toutes les entités
         $manager->flush();
     }
 }
