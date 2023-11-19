774a7dccf6d5b33d91c1bb7f0f0f8dbfb60a39a4
Joyce <lapinette2002@ymail.com>
2023-11-19 13:31:14 +0100

    création de la page qui liste les plans

--- src/Controller/WorkoutPlanController.php
+++ src/Controller/WorkoutPlanController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\WorkoutPlanRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WorkoutPlanController extends AbstractController
 {
-    #[Route('/workout/plan', name: 'app_workout_plan')]
+    #[Route('/workoutplan', name: 'app_workout_plan')]
     public function index(): Response
     {
         return $this->render('workout_plan/index.html.twig', [
             'controller_name' => 'WorkoutPlanController',
+        ]);
+    }
+
+    #[Route('/workoutplan/add', name: 'app_workout_plan_add')]
+    public function add(): Response
+    {
+        return $this->render('workout_plan/add.html.twig', [
+            'controller_name' => 'WorkoutPlanController',
+        ]);
+    }
+
+    #[Route('/workoutplan/list', name: 'app_workout_plan_list')]
+    public function list(WorkoutPlanRepository $workoutPlanRepository): Response
+    {
+        $workoutPlans = $workoutPlanRepository->findAll();
+
+
+        return $this->render('workout_plan/list.html.twig', [
+            'controller_name' => 'WorkoutPlanController',
+            'workoutPlans' => $workoutPlans,
         ]);
     }
 }
